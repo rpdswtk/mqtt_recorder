2db994eb13aabb0c72a065e241b5ff4d1eeaf6dc
rpdswtk <rpdswtk@github.com>
2020-07-05 16:49:07 +0200

    Changed name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mqtt-recorder-RPDSWTK",
+    name="mqtt-recorder",
     version="1.0.0",
     author="RPDSWTK",
     description="MQTT recorder tool",
